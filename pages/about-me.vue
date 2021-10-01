d7b4aa930597a9c72a9856a9d5d48964f2df2efa
Chicle <chicle@protonmail.com>
2021-10-01 05:57:08 +0200

    Added about me page (empty)

---
+++ pages/about-me.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mx-auto py-8 px-5">
+    <div
+      class="
+        dark:text-white
+        text-gray-700
+        font-bold
+      "
+    >
+    TODO
+    </div>
+  </div>
+</templxdate>

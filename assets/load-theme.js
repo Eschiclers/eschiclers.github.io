23e50c79c480f496d33437fb08704e34ccf058d2
Chicle <chicle@protonmail.com>
2021-10-05 11:27:57 +0200

    Removed unused asset script "load-theme"

--- assets/load-theme.js
+++
@@ -1,5 +0,0 @@
-if(localStorage.getItem("dark") === "true" || (!('dark' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.documentElement.classList.add("dark");
-} else {
-  document.documentElement.classList.remove("dark");
-}
